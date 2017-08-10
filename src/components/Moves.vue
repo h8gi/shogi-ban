b903f6a6aee85b5e42db48436106b87e7cf0f540
yagi <hiro.yagi0245@gmail.com>
2017-08-11 07:24:36 +0900

    create moves component

---
+++ src/components/Moves.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="moves">
+    <div v-for="m in contents">
+      {{m}}
+    </div>
+  </div>
+</template>
+
+<script>
+import syogi from '@/syogi-lib'
+export default {
+  name: 'moves',
+  components: {
+  },
+  props: {
+    contents: {
+      type: Array
+    }
+  },
+  data () {
+    return {    
+    }
+  },
+  computed: {
+  },
+  methods: {    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.moves {
+
+}
+</style>

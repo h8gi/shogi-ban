0df4ef97382d6da80fac5d4c1994f47e70f3d7dd
yagi <hiro.yagi0245@gmail.com>
2017-08-11 07:25:25 +0900

    ask promote with alertify

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../node_modules/alertifyjs/build/css/alertify.min.css';
+@import '../node_modules/alertifyjs/build/css/themes/default.min.css';
+.alertify .ajs-dimmer, .alertify .ajs-modal {
+  -webkit-transition-duration: 80ms;
+  transition-duration: 80ms;
+}
 #app {
 }
 </style>

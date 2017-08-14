8e5830c4bb2fcb02d948026b760e7960fb8a4475
yagi <hiro.yagi0245@gmail.com>
2017-08-15 05:22:20 +0900

    rename moves to movelist

---
+++ src/components/MoveList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="move-list">
+    <div v-for="m in contents">
+      {{m}}
+    </div>
+    <button>&lt;</button>
+    <button>&gt;</button>
+  </div>
+</template>
+
+<script>
+import syogi from '@/syogi-lib'
+export default {
+  name: 'move-list',
+  components: {
+  },
+  props: {
+    contents: {
+      type: Array
+    }
+  },
+  data () {
+    return {    
+    }
+  },
+  computed: {
+  },
+  methods: {    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.move-list {
+
+}
+</style>
